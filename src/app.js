00aaa9bafd4913b8f68b1ce964b94f7a844aa195
Arpan Patel <adp8ke@gmail.com>
2019-07-26 15:41:43 -0400

    client is successfully linked up

--- src/app.js
+++ src/app.js
@@ -17,8 +17,8 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.use('/api/folders', foldersRouter)
-app.use('/api/notes', notesRouter)
+app.use('/folders', foldersRouter)
+app.use('/notes', notesRouter)
 
 // app.get('/', (req,res)=>{
 //     res.send('Hello, world!')

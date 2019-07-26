673665b4b8169569e22f2f1c312e6e36e334635d
Arpan Patel <adp8ke@gmail.com>
2019-07-26 14:51:59 -0400

    finished folder router and working on postman

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO folders (folder_title)
+VALUES
+('Folder 1'),
+('Folder 2'),
+('Folder 3');
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,5 @@
+INSERT INTO notes (note_title, content, folder_id)
+VALUES
+('Note 1', 'Seed note content', 1),
+('Note 2', 'Seed note content', 2),
+('Note 3', 'Seed note content', 3);
